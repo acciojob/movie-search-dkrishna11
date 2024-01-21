f3d10361e5a3db3d104d9929b6aa5efb1db2ce1a
dkrishna11 <dkrishna051997@gmail.com>
2024-01-21 22:32:19 +0530

    com

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import { movies } from "./movies";
 
 const App = () => {
+  const [search, setSearch] = useState();
+
+  function handleSearch() {
+    const serchedMovie = [...movies].filter((movie) =>
+      movie.name.toLowerCase().includes(search)
+    );
+    setSearch(serchedMovie);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input type="text" onChange={handleSearch} value={search} />
+      {search ? (
+        <div>
+          {search.map((movie, index) => (
+            <div key={index}>
+              <li>{movie.name}</li>
+              <img src={movie.img} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Invalid movie name. Please try again</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/movies.js
@@ -0,0 +1,34 @@
+export default movies = [
+  {
+    name: "Main Atal Hoon",
+    img: "https://assets.gadgets360cdn.com/pricee/assets/product/202301/Main-Atal-Hoon_1673000994.jpg?downsize=215:301",
+  },
+  {
+    name:"Merry Christmas",
+    img:"https://assets.gadgets360cdn.com/pricee/assets/product/202311/Merry-Christmas_1700138579.jpg?downsize=215:301",
+  },
+  {
+    name:"Kho Gaye Hum Kahan",
+    img:"https://assets.gadgets360cdn.com/pricee/assets/product/202312/Kho_Gaye_Hum_Kahan1_1701859058.jpg?downsize=215:301",
+  },
+  {
+    name:"Dry Day",
+    img:"https://assets.gadgets360cdn.com/pricee/assets/product/202312/Dry_Day1_1702638213.jpg?downsize=215:301",
+  },
+  {
+    name:"Dunki",
+    img:"https://assets.gadgets360cdn.com/pricee/assets/product/202311/Dunki_1699944194.jpg?downsize=215:301",
+  },
+  {
+    name:"Hi Papa",
+    img:"https://assets.gadgets360cdn.com/pricee/assets/product/202307/Hi_Papa_1689847279.jpg?downsize=215:301",
+  },
+  {
+    name:"Animal",
+    img:"https://assets.gadgets360cdn.com/pricee/assets/product/202301/Animal_1673270841.jpg?downsize=215:301",
+  },
+  {
+    name:"Sam Bahadur",
+    img:"https://assets.gadgets360cdn.com/pricee/assets/product/202311/Sam-Bahadur_1699944687.jpg?downsize=215:301",
+  },
+];
